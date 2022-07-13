0abe01b80d368d15e318e86d7065445c5f447168
Anil Sharma <Anil Sharma@LAPTOP-H47NI489>
2022-07-13 12:03:27 +0530

    first commit

--- corejava/src/corejava_package/FunctionalInterface.java
+++ corejava/src/corejava_package/FunctionalInterface.java
@@ -8,8 +8,8 @@ public class FunctionalInterface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int x=8;
-cube c=(int a)->a*a*a;
+int x=5;
+cube c=(int a)->a+a;
 int result=c.calculate(x);
 System.out.println(result);
 
---
+++ corejava/src/corejava_package/InterfaceRunnable.java
@@ -0,0 +1,24 @@
+package corejava_package;
+interface Runnable
+{
+	public void run(int speed);
+
+}
+
+public class InterfaceRunnable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int speed=150;
+Runnable r=(carSpeed)->
+{
+	System.out.println("Running at the speed of:"+carSpeed);	
+};
+r.run(speed);
+Runnable r1=(int carSpeed)->{
+	System.out.println("Running at the speed of:"+carSpeed);
+}; 
+r1.run(speed);
+	}
+
+}
