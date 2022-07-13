a38917feb1821fb48d388813e3bb16142cea00fb
Anil Sharma <Anil Sharma@LAPTOP-H47NI489>
2022-07-13 11:12:19 +0530

    first commit

---
+++ corejava/src/corejava_package/FunctionalInterface.java
@@ -0,0 +1,19 @@
+package corejava_package;
+interface cube
+{
+	int calculate(int a);
+
+}
+public class FunctionalInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=8;
+cube c=(int a)->a*a*a;
+int result=c.calculate(x);
+System.out.println(result);
+
+		
+	}
+
+}
